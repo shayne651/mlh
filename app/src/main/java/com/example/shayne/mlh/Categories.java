1c29d725c67b74293bc11b0379370616287c7674
Robert Marshall <robert.marshall1996@gmail.com>
2017-12-02 18:05:55 -0500

    grade list layout

---
+++ app/src/main/java/com/example/shayne/mlh/Categories.java
@@ -0,0 +1,23 @@
+package com.example.shayne.mlh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/*4*
+ * Created by Robert on 2017-12-02.
+ */
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+
+
+
+}
+
--- app/src/main/java/com/example/shayne/mlh/Data.kt
+++ app/src/main/java/com/example/shayne/mlh/Data.kt
@@ -6,38 +6,42 @@ import kotlin.collections.ArrayList
 /**
  * Created by andrei on 12/2/17.
  */
-data class Grade (var name: String, var weight: Float,
-                  internal var totalComponents: Int, internal var drop_lowest: Int = 0,
-                  private var _components: Array<Float>, var mark: Float = 0f,
-                  var totalMark: Float = 0f): Serializable {
+data class Grade(var name: String, var weight: Float,
+                 internal var totalComponents: Int, internal var drop_lowest: Int = 0,
+                 private var _components: Array<Float>, var mark: Float = 0f,
+                 var totalMark: Float = 0f) : Serializable {
     var components: Array<Float>
         get() = _components
         set(components) {
-            val sum = components.sum()
-            if  (totalComponents < components.size)
-                totalComponents = components.size
-            mark = sum/components.size
-            totalMark = sum/totalComponents
+            val size = components.size
+            val validMarks = components.sortedArray().sliceArray(drop_lowest until size)
+            val sum = validMarks.sum()
+            if (totalComponents < size)
+                totalComponents = size
+
+            mark = sum / size
+            totalMark = sum / totalComponents
+
             _components = components
         }
 }
 
-data class Class (var name: String,
-                  val assignments: ArrayList<Grade> = ArrayList<Grade>()): Serializable
+data class Class(var name: String,
+                 val assignments: ArrayList<Grade> = ArrayList<Grade>()) : Serializable
 
-data class Semsester  (var name: String,
-                       val classes: ArrayList<Class> = ArrayList<Class>()): Serializable
+data class Semsester(var name: String,
+                     val classes: ArrayList<Class> = ArrayList<Class>()) : Serializable
 
-fun newGrade(name: String, weight: Float, components: Array<Float>) : Grade {
+fun newGrade(name: String, weight: Float, components: Array<Float>): Grade {
     val sum = components.sum()
-    val mark = sum/components.size
+    val mark = sum / components.size
     return Grade(name, weight, components.size, _components = components, mark = mark)
 }
 
-fun newGrade(name: String, weight: Float, components: Array<Float>, totalComponents: Int) : Grade {
+fun newGrade(name: String, weight: Float, components: Array<Float>, totalComponents: Int): Grade {
     val sum = components.sum()
-    val mark = sum/components.size
-    val totalMark = sum/totalComponents
+    val mark = sum / components.size
+    val totalMark = sum / totalComponents
     var count = totalComponents
     if (totalComponents < components.size)
         count = components.size
@@ -45,42 +49,55 @@ fun newGrade(name: String, weight: Float, components: Array<Float>, totalCompone
             totalMark = totalMark)
 }
 
+fun newGrade(name: String, weight: Float, components: Array<Float>, totalComponents: Int, drop_lowest: Int): Grade {
+    val size = components.size
+    val validMarks = components.sortedArray().sliceArray(drop_lowest until size)
+    val sum = validMarks.sum()
+    val mark = sum / components.size
+    val totalMark = sum / totalComponents
+    var count = totalComponents
+    if (totalComponents < components.size)
+        count = components.size
+    return Grade(name, weight, count, _components = components, mark = mark,
+            totalMark = totalMark, drop_lowest = drop_lowest)
+}
 
-fun newClass(name: String) : Class {
+
+fun newClass(name: String): Class {
     return Class(name)
 }
 
-fun newSemester(name: String) : Semsester {
+fun newSemester(name: String): Semsester {
     return Semsester(name)
 }
 
-fun addClass(s:Semsester, c:Class): Semsester {
+fun addClass(s: Semsester, c: Class): Semsester {
     s.classes.add(c)
     return s
 }
 
 
-fun addClass(s:Semsester, name:String): Semsester {
+fun addClass(s: Semsester, name: String): Semsester {
     s.classes.add(Class(name))
     return s
 }
 
-fun addGrade(c:Class, g:Grade): Class  {
+fun addGrade(c: Class, g: Grade): Class {
     c.assignments.add(g)
     return c;
 }
 
-fun addGrade(c:Class, name: String, weight: Float, components: Array<Float>): Class  {
-    c.assignments.add(newGrade(name,weight, components))
+fun addGrade(c: Class, name: String, weight: Float, components: Array<Float>): Class {
+    c.assignments.add(newGrade(name, weight, components))
     return c;
 }
 
-fun gradeNeeded(c: Class) : Float {
+fun gradeNeeded(c: Class): Float {
     return gradeNeeded(c, 0.5f)
 }
 
-fun gradeNeeded(c: Class, target: Float) : Float {
-    return (target - avg(c))/(1-c.assignments.map { it.weight }.sum())
+fun gradeNeeded(c: Class, target: Float): Float {
+    return (target - avg(c)) / (1 - c.assignments.map { it.weight }.sum())
 }
 
 fun Boolean.toInt() = if (this) 1 else 0
