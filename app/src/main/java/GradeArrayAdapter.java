7f3dc251fe654fb68f3aaf66dc5693317c246f9d
shayne <shayne651@gmail.com>
2017-12-02 13:02:27 -0500

    Adding in the base for main activity, started making the list item for the list view, base for grades

---
+++ app/src/main/java/GradeArrayAdapter.java
@@ -0,0 +1,19 @@
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.example.shayne.mlh.Grades;
+import com.example.shayne.mlh.R;
+
+import java.util.List;
+
+/**
+ * Created by shayne on 2017-12-02.
+ */
+
+public class GradeArrayAdapter extends ArrayAdapter<Grades> {
+
+    public GradeArrayAdapter(Context context, List<Grades>){
+        super(context, R.layout.activity_list_item);
+    }
+
+}
---
+++ app/src/main/java/com/example/shayne/mlh/Grades.java
@@ -0,0 +1,8 @@
+package com.example.shayne.mlh;
+
+/**
+ * Created by shayne on 2017-12-02.
+ */
+
+public class Grades {
+}
