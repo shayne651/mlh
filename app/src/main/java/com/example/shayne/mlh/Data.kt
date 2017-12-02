dc826a6dbf7700562c93c85a616feaa1aceb128b
Andrei Stoica <andrei.stoica.365@gmail.com>
2017-12-02 15:20:20 -0500

    added functions for creating new semesters, classes, and adding classes and grades

--- app/src/main/java/com/example/shayne/mlh/Data.kt
+++ app/src/main/java/com/example/shayne/mlh/Data.kt
@@ -1,15 +1,34 @@
 package com.example.shayne.mlh
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by andrei on 12/2/17.
  */
 data class Grade (var name: String, var mark: Float, var weight: Float)
 
-data class Class (var name: String, var assignments: ArrayList<Grade>)
+data class Class (var name: String, val assignments: ArrayList<Grade> = ArrayList<Grade>())
 
-data class Semsester  (var name: String, var classes: ArrayList<Class>)
+data class Semsester  (var name: String, val classes: ArrayList<Class> = ArrayList<Class>())
+
+fun newClass(name: String) : Class {
+    return Class(name)
+}
+
+fun newSemester(name: String) : Semsester {
+    return Semsester(name)
+}
+
+fun addClass(s:Semsester, c:Class): Semsester {
+    s.classes.add(c)
+    return s
+}
+
+fun addGrade(c:Class, g:Grade): Class  {
+    c.assignments.add(g)
+    return c;
+}
 
 fun avg(c: Class): Float {
     return c.assignments.map { it.mark * it.weight }.sum() / c.assignments.size
